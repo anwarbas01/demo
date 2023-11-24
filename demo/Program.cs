789bcc1b30b387f43371cd1398ff2e946033a485
anwar0120 <anwarbas07@gmail.com>
2023-11-24 17:24:36 +0530

    update

--- demo/Program.cs
+++ demo/Program.cs
@@ -10,7 +10,7 @@ namespace demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello anwar");
 
         }
     }
